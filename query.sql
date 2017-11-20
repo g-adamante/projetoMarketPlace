5f4ca5c0996d49c8ef4c123b91ae3438fa212a34
g-adamante <gabriel.henrique.a@gmail.com>
2017-11-19 22:32:56 -0200

    Update query.sql

--- query.sql
+++ query.sql
@@ -9,34 +9,30 @@ user_pass   VARCHAR(255) NOT NULL,
 user_email  VARCHAR(255) NOT NULL,
 user_creation_date   TIMESTAMP NOT NULL,
 user_type  number(8) NOT NULL,
-CONSTRAINT user_id_pk PRIMARY KEY (user_id),
-CONSTRAINT user_email_unique UNIQUE (user_email),
-CONSTRAINT user_name_unique UNIQUE (user_name)
+PRIMARY KEY (user_id)
 );
 
 
 CREATE TABLE topics (
 topic_id        number(8) NOT NULL,
 topic_subject       VARCHAR(255) NOT NULL,
-topic_message       CLOB NOT NULL,
 topic_date      TIMESTAMP NOT NULL,
 topic_cat       number(8) NOT NULL,
 topic_by        number(8) NOT NULL,
-topic_lawyer    number(8),
-topic_status    number(8),
+topic_closed    number(1),
 PRIMARY KEY (topic_id),
 FOREIGN KEY(topic_by) REFERENCES users(user_id)
 );
 
-CREATE TABLE REPLIES (
-reply_id         number(8) NOT NULL,
-reply_content        CLOB NOT NULL,
-reply_date       TIMESTAMP NOT NULL,
-reply_topic      number(8) NOT NULL,
-reply_by     NUMBER(8) NOT NULL,
-PRIMARY KEY (reply_id),
-FOREIGN KEY(reply_topic) REFERENCES topics(topic_id),
-FOREIGN KEY(reply_by) REFERENCES users(user_id)
+CREATE TABLE posts (
+post_id         number(8) NOT NULL,
+post_content        CLOB NOT NULL,
+post_date       TIMESTAMP NOT NULL,
+post_topic      number(8) NOT NULL,
+post_by     NUMBER(8) NOT NULL,
+PRIMARY KEY (post_id),
+FOREIGN KEY(post_topic) REFERENCES topics(topic_id),
+FOREIGN KEY(post_by) REFERENCES users(user_id)
 );
 
 DROP SEQUENCE USER_SEQ;
@@ -57,8 +53,8 @@ CREATE SEQUENCE TOPIC_SEQ
   NOCACHE
   NOCYCLE;
 
-DROP SEQUENCE  REPLIES_SEQ;
-CREATE SEQUENCE REPLIES_SEQ
+DROP SEQUENCE POSTS_SEQ;
+CREATE SEQUENCE POSTS_SEQ
   MINVALUE 1
   MAXVALUE 9999999999
   START WITH 1
